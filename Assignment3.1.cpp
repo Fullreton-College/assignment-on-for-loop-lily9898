5f3161e7f8246eaf9a6a9bbe1cdacc2f6a5b7677
Kim, Lily <choiheewoo@gmail.com>
2026-03-01 16:01:44 -0800

    Code changes complete

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,33 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
+
+int firstNum, secondNum;
 
 int main()
 {
+
+cout << "Input first integer: ";
+cin >> firstNum;
+
+cout << "Input second integer: ";
+cin >> secondNum;
+
+if (secondNum < firstNum )
+{
+    cout << "Second integer can't be less than the first.";
+}
+
+else
+{
+    for (int i = firstNum; i <= secondNum; i += 5)
+    {
+        cout << i << " ";  
+    }
+}
+
+cout << endl;
 
 return 0;
 }
